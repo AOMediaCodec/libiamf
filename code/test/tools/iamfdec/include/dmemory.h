5b0d48f86011b2f4db511a741508e5fb7b1db8ef
yilun.zhang <yilun.zhang@samsung.com>
2024-06-26 11:06:17 +0800

    Improve the license comments in source code

--- code/test/tools/iamfdec/include/dmemory.h
+++ code/test/tools/iamfdec/include/dmemory.h
@@ -1,37 +1,14 @@
 /*
-BSD 3-Clause Clear License The Clear BSD License
-
-Copyright (c) 2023, Alliance for Open Media.
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-
-1. Redistributions of source code must retain the above copyright notice, this
-   list of conditions and the following disclaimer.
-
-2. Redistributions in binary form must reproduce the above copyright notice,
-   this list of conditions and the following disclaimer in the documentation
-   and/or other materials provided with the distribution.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+* $Id: debug.h,v 1.5 2006/01/30 23:07:57 mclark Exp $
+*
+* Copyright (c) 2004, 2005 Metaparadigm Pte. Ltd.
+* Michael Clark <michael@metaparadigm.com>
+* Copyright (c) 2009 Hewlett-Packard Development Company, L.P.
+*
+* This library is free software; you can redistribute it and/or modify
+* it under the terms of the MIT license. See COPYING for details.
+*
 */
-
-/**
- * @file dmemory.h
- * @brief Memory malloc and free.
- * @version 0.1
- * @date Created 03/03/2023
- **/
-
 
 #ifndef _DEBUG_H_
 #define _DEBUG_H_
