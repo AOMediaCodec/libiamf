ad1542248cec7c0d8e1f2e8bdc72ea51f9d3c72b
yilun.zhang <yilun.zhang@samsung.com>
2024-07-18 20:24:51 +0800

    Update error and warning logs for verification

--- code/include/vlogging_tool_sr.h
+++ code/include/vlogging_tool_sr.h
@@ -18,6 +18,7 @@
  **/
 
 #include <stdarg.h>
+#include <stdint.h>
 
 #ifndef _VLOGGING_TOOL_SR_H_
 #define _VLOGGING_TOOL_SR_H_
@@ -34,6 +35,7 @@ int vlog_print(LOG_TYPE type, uint64_t key, const char* format, ...);
 int vlog_obu(uint32_t obu_type, void* obu,
              uint64_t num_samples_to_trim_at_start,
              uint64_t num_samples_to_trim_at_end);
+int vlog_decop(char* decop_text);
 int vlog_file_close();
 int is_vlog_file_open();
 
--- code/src/common/IAMF_debug.h
+++ code/src/common/IAMF_debug.h
@@ -20,10 +20,23 @@
 #ifndef IAMF_DEBUG_H
 #define IAMF_DEBUG_H
 
-#ifdef IA_DBG
+#ifndef SUPPORT_VERIFIER
+#define SUPPORT_VERIFIER 0
+#endif
 
 #include <stdio.h>
 #include <string.h>
+
+#if SUPPORT_VERIFIER
+#include "vlogging_tool_sr.h"
+#endif
+
+#ifndef __MODULE__
+#define __MODULE__ \
+  (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
+#endif
+
+#ifdef IA_DBG
 
 #ifndef IA_TAG
 #define IA_TAG "IAMF"
@@ -44,26 +57,51 @@
 #define obu_dump(a, b, c)
 #endif
 
-#ifndef __MODULE__
-#define __MODULE__ \
-  (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
-#endif
-
 #ifdef WIN32
+#if SUPPORT_VERIFIER
+#define ia_log(level, slevel, fmt, ...)                                     \
+  do {                                                                      \
+    if ((IA_DBG_E & level) || (IA_DBG_W & level)) {                         \
+      char text[2048];                                                      \
+      sprintf(text, "[%-9s:%s(%4d):%s]>" fmt, slevel, __MODULE__, __LINE__, \
+              __FUNCTION__, ##__VA_ARGS__);                                 \
+      vlog_decop(text);                                                     \
+    }                                                                       \
+    if (IA_DBG_LEVEL & level) {                                             \
+      fprintf(stderr, "[%-9s:%s:%s(%4d):%s]>" fmt "\n", IA_TAG, slevel,     \
+              __MODULE__, __LINE__, __FUNCTION__, ##__VA_ARGS__);           \
+    }                                                                       \
+  } while (0)
+#else  // !SUPPORT_VERIFIER
 #define ia_log(level, slevel, fmt, ...)                                 \
   do {                                                                  \
-    if (IA_DBG_LEVEL & level) {                                             \
+    if (IA_DBG_LEVEL & level) {                                         \
       fprintf(stderr, "[%-9s:%s:%s(%4d):%s]>" fmt "\n", IA_TAG, slevel, \
               __MODULE__, __LINE__, __FUNCTION__, ##__VA_ARGS__);       \
     }                                                                   \
   } while (0)
-
+#endif
 #define ia_loge(fmt, ...) ia_log(IA_DBG_E, "ERROR", fmt, __VA_ARGS__)
 #define ia_logw(fmt, ...) ia_log(IA_DBG_W, "WARN ", fmt, __VA_ARGS__)
 #define ia_logi(fmt, ...) ia_log(IA_DBG_I, "INFO ", fmt, __VA_ARGS__)
 #define ia_logd(fmt, ...) ia_log(IA_DBG_D, "DEBUG", fmt, __VA_ARGS__)
 #define ia_logt(fmt, ...) ia_log(IA_DBG_T, "TRACE", fmt, __VA_ARGS__)
 #else
+#if SUPPORT_VERIFIER
+#define ia_log(level, slevel, fmt, arg...)                              \
+  do {                                                                  \
+    if ((IA_DBG_E & level) || (IA_DBG_W & level)) {                     \
+      char text[2048];                                                  \
+      sprintf(text, "[%-9s:%s(%4d):%s]>" fmt "\n", slevel, __MODULE__,  \
+              __LINE__, __FUNCTION__, ##arg);                           \
+      vlog_decop(text);                                                 \
+    }                                                                   \
+    if (IA_DBG_LEVEL & level) {                                         \
+      fprintf(stderr, "[%-9s:%s:%s(%4d):%s]>" fmt "\n", IA_TAG, slevel, \
+              __MODULE__, __LINE__, __FUNCTION__, ##arg);               \
+    }                                                                   \
+  } while (0)
+#else  // !SUPPORT_VERIFIER
 #define ia_log(level, slevel, fmt, arg...)                              \
   do {                                                                  \
     if (IA_DBG_LEVEL & level) {                                         \
@@ -71,7 +109,7 @@
               __MODULE__, __LINE__, __FUNCTION__, ##arg);               \
     }                                                                   \
   } while (0)
-
+#endif
 #define ia_loge(fmt, arg...) ia_log(IA_DBG_E, "ERROR", fmt, ##arg)
 #define ia_logw(fmt, arg...) ia_log(IA_DBG_W, "WARN ", fmt, ##arg)
 #define ia_logi(fmt, arg...) ia_log(IA_DBG_I, "INFO ", fmt, ##arg)
@@ -81,14 +119,37 @@
 
 #else
 #ifdef WIN32
+#if SUPPORT_VERIFIER
+#define ia_log(level, slevel, fmt, ...)                                   \
+  do {                                                                    \
+    char text[2048];                                                      \
+    sprintf(text, "[%-9s:%s(%4d):%s]>" fmt, slevel, __MODULE__, __LINE__, \
+            __FUNCTION__, ##__VA_ARGS__);                                 \
+    vlog_decop(text);                                                     \
+  } while (0)
+#define ia_loge(fmt, ...) ia_log(IA_DBG_E, "ERROR", fmt, __VA_ARGS__)
+#define ia_logw(fmt, ...) ia_log(IA_DBG_W, "WARN ", fmt, __VA_ARGS__)
+#else  // !SUPPORT_VERIFIER
 #define ia_loge(fmt, ...)
 #define ia_logw(fmt, ...)
+#endif
 #define ia_logi(fmt, ...)
 #define ia_logd(fmt, ...)
 #define ia_logt(fmt, ...)
-#else
-#define ia_loge(fmt, arg...)
-#define ia_logw(fmt, arg...)
+#else  // !WIN32
+#if SUPPORT_VERIFIER
+#define ia_log(level, slevel, fmt, arg...)                                     \
+  do {                                                                         \
+    char text[2048];                                                           \
+    sprintf(text, "[%-9s:%s(%4d):%s]>" fmt "\n", slevel, __MODULE__, __LINE__, \
+            __FUNCTION__, ##arg);                                              \
+  } while (0)
+#define ia_loge(fmt, arg...) ia_log(IA_DBG_E, "ERROR", fmt, ##arg)
+#define ia_logw(fmt, arg...) ia_log(IA_DBG_W, "WARN ", fmt, ##arg)
+#else  // !SUPPORT_VERIFIER
+#define ia_loge(fmt, ...)
+#define ia_logw(fmt, ...)
+#endif
 #define ia_logi(fmt, arg...)
 #define ia_logd(fmt, arg...)
 #define ia_logt(fmt, arg...)
